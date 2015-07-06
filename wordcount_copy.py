f590e9851769d431a9fc9fc3bb6ba3058c074583
Hackbright Student <info@hackbrightacademy.com>
2015-07-06 16:53:21 -0700

    Print dictionary of words correctly

--- wordcount_copy.py
+++ wordcount_copy.py
@@ -1,11 +1,27 @@
 # put your code here
-open_file = open("test.txt")
-file_dict = {}
+def word_count(filename):
+    open_file = open(filename)
+    file_dict = {}
 
-def word_count(open_file):
     for line in open_file:
         line = line.rstrip()
         words = line.split(' ')
+
+        for word in words:
+            if word not in file_dict:
+                file_dict[word] = 1
+            else:
+                file_dict[word] = file_dict[word] + 1
+
+    for word, number in file_dict.items():
+                #print file_dict.get()
+        #ey = word
+        #value = file_dict[word]
+        print "%s %d" %(word, number)
+    return file_dict
+
+#word_count("test.txt")
+word_count("twain.txt")
 
 
 """
@@ -14,11 +30,3 @@ def word_count(open_file):
     else: #if the word is already in the current dictionary
         increase value by 1
 """
-        for word in words:
-            if word not in file_dict:
-                file_dict[word] = 1
-            else:
-                file_dict[word] = file_dict[word] + 1
-    print file_dict
-
-word_count(open_file)
